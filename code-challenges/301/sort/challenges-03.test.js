c06801e6aba6606da52a1d8f21b1613404b93728
mspake <morgana.spake@gmail.com>
2019-04-10 17:53:56 -0700

    third test passing

--- code-challenges/301/sort/challenges-03.test.js
+++ code-challenges/301/sort/challenges-03.test.js
@@ -41,7 +41,12 @@ Write a function named sortByLength that takes in an array of strings and return
 ------------------------------------------------------------------------------------------------ */
 
 const sortByLength = (arr) => {
-  // Solution code here...
+  arr.sort((a, b) => {
+    if (a.length > b.length) return 1;
+    if (a.length < b.length) return -1;
+    return 0;
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -190,7 +195,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should sort strings by length', () => {
     const ans = sortByLength(['alphabet', 'Zebra', 'Alphabet', 'carrot']);
     expect(ans.slice(0, 2)).toStrictEqual(['Zebra', 'carrot']);
