9d2c56247618e2f5c1459d2cd4a8f1d600259209
mspake <morgana.spake@gmail.com>
2019-04-14 16:16:15 -0700

    ninth test passing

--- code-challenges/301/split-join-slice-splice/challenges-05.test.js
+++ code-challenges/301/split-join-slice-splice/challenges-05.test.js
@@ -188,7 +188,8 @@ For example, removeVowels('gregor') returns 'grgr'.
 ------------------------------------------------------------------------------------------------ */
 
 const removeVowels = (str) => {
-  // Solution code here...
+  const letters = str.split(/a|e|i|o|u/g);
+  return letters.join('');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -290,7 +291,7 @@ describe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   test('It should return the string without vowels', () => {
     expect(removeVowels('gregor')).toStrictEqual('grgr');
     expect(removeVowels('gregor').length).toStrictEqual(4);
