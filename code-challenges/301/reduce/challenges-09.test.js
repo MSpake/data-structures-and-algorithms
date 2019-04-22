17bc2e2dc457877998dc100f3679eb38e0c9a0e4
mspake <morgana.spake@gmail.com>
2019-04-22 08:19:22 -0700

    seventh test passing

--- code-challenges/301/reduce/challenges-09.test.js
+++ code-challenges/301/reduce/challenges-09.test.js
@@ -231,7 +231,11 @@ const snorlaxData = {
 };
 
 const extractStat = (statName, arr) => {
-  // Solution code here...
+  let statMatch = arr.reduce((accumulator, val) => {
+    if (val.stat.name === statName) accumulator = val;
+    return accumulator;
+  }, {});
+  return statMatch;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -295,7 +299,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return any stats that match the input', () => {
     expect(extractStat('speed', snorlaxData.stats)).toStrictEqual({ stat: { url: 'https://pokeapi.co/api/v2/stat/6/', name: 'speed' }, effort: 5, baseStat: 30 });
   });
