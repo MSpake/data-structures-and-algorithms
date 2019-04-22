a1af1d1a4160805a0b0d724a6870728bf0dcdd9b
mspake <morgana.spake@gmail.com>
2019-04-22 08:07:35 -0700

    fifth test passing

--- code-challenges/301/reduce/challenges-09.test.js
+++ code-challenges/301/reduce/challenges-09.test.js
@@ -159,7 +159,12 @@ Hint: The accumulator should begin as { count: 0, sum: 0 }
 ------------------------------------------------------------------------------------------------ */
 
 const calculateAverage = (arr) => {
-  // Solution code here...
+  let totals = arr.reduce((accumulator, val) => {
+    accumulator.count++;
+    accumulator.sum += val;
+    return accumulator;
+  }, { count: 0, sum: 0 });
+  return (totals.sum / totals.count);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -274,7 +279,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return the average of the numbers in the array', () => {
     expect(calculateAverage([18, 290, 37, 4, 55, 16, 7, 85])).toStrictEqual(64);
   });
