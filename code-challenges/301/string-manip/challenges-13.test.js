a96a4d2a0f02720a122b6eaadf1afca458959270
mspake <morgana.spake@gmail.com>
2019-04-25 07:47:52 -0700

    fifth test passing

--- code-challenges/301/string-manip/challenges-13.test.js
+++ code-challenges/301/string-manip/challenges-13.test.js
@@ -66,7 +66,9 @@ Write a function named allHappy that takes in an array of strings and returns a 
 ------------------------------------------------------------------------------------------------ */
 
 const allHappy = (arr) => {
-  // Solution code here...
+  const happy = arr.map(string => string.includes(':)'));
+  if (happy.includes(false)) return false;
+  return true;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -194,7 +196,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should correctly assess whether all the strings are happy', () => {
     const words = ['things', 'apple (:)', ':)banana', 'missing that thing', 'cant:)aloupe'];
 
