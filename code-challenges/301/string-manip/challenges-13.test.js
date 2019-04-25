f2dc323ef8414d315c93f16e440dbdb3e332d7eb
mspake <morgana.spake@gmail.com>
2019-04-25 07:49:22 -0700

    sixth test passing

--- code-challenges/301/string-manip/challenges-13.test.js
+++ code-challenges/301/string-manip/challenges-13.test.js
@@ -78,7 +78,11 @@ Write a function named findAnything that takes in an array of strings, along wit
 ------------------------------------------------------------------------------------------------ */
 
 const findAnything = (arr, target) => {
-  // Solution code here...
+  return arr.filter(string => {
+    if (string.includes(target)) {
+      return string;
+    }
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -206,7 +210,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should find all the strings that contain a given string', () => {
     const words = ['things', 'apple (:)', ':)banana', 'missing that thing', 'cant:)aloupe'];
 
