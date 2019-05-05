a6095b0b29cbad05a2ebc9f227c13f44f5228565
mspake <morgana.spake@gmail.com>
2019-05-04 22:42:03 -0700

    challenge 9 (reduce) skipped test passing

--- code-challenges/301/reduce/challenges-09.test.js
+++ code-challenges/301/reduce/challenges-09.test.js
@@ -91,7 +91,11 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 ------------------------------------------------------------------------------------------------ */
 
 const reversedString = (arr) => {
-  // Solution code here...
+  let reversed = Array.from(arr).reduce((accumulator, letter) => {
+    accumulator = letter + accumulator;
+    return accumulator;
+  }, '');
+  return reversed;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -280,7 +284,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should return the string with the characters in reverse order', () => {
     expect(reversedString('Code 301')).toStrictEqual('103 edoC');
   });
