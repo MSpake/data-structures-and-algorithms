f7c1b384441e3844b2bfed2dd84e8507926b6370
Morgana Spake <morgana.spake@gmail.com>
2019-06-20 08:46:42 -0700

    code

--- code-challenges/401/findPair/findPair.test.js
+++ code-challenges/401/findPair/findPair.test.js
@@ -4,10 +4,14 @@ const { findWithLoops, findWithMap } = require('./findPair.js');
 
 describe('Find Pair', () => {
   it('Can find by looping (brute force)', () => {
-
+    const testArray = [3,5,2,9,1];
+    const num = 6;
+    expect(findWithLoops(testArray, num)).toEqual([5,1]);
   });
 
   it('Can find using map', () => {
-
+    const testArray = [3,5,2,9,1];
+    const num = 11;
+    expect(findWithMap(testArray, num)).toEqual([2,9]);
   });
 });
