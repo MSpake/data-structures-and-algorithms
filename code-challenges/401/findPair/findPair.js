d9d6de51b49662a0317e6ab2ac125da8edf6c6fb
Morgana Spake <morgana.spake@gmail.com>
2019-06-19 23:49:13 -0700

    readme and whiteboard

---
+++ code-challenges/401/findPair/findPair.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function findWithLoops(arr, num) {
+
+}
+
+function findWithMap(arr, num) {
+  
+}
+
+module.exports = {
+  findWithLoops,
+  findWithMap,
+};
---
+++ code-challenges/401/findPair/findPair.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { findWithLoops, findWithMap } = require('./findPair.js');
+
+describe('Find Pair', () => {
+  it('Can find by looping (brute force)', () => {
+
+  });
+
+  it('Can find using map', () => {
+
+  });
+});
